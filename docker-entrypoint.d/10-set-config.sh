4cb75636246e0f682031ce73c3b471334f37690d
Niels Højen <niels@hojen.net>
2023-02-28 14:32:54 +0100

    Update 10-set-config.sh

--- docker-entrypoint.d/10-set-config.sh
+++ docker-entrypoint.d/10-set-config.sh
@@ -2,14 +2,25 @@
 
 sleep 5
 
-if [[ ${TLS} = true ]]; then
-  SERVER_URL="https://localhost:443"
+execMdmctl="/usr/local/bin/mdmctl config set -api-token ${API_KEY} -name production -server-url ${SERVER_URL}"
+
+if [[ ${MICROMDM_API_KEY} ]]; then
+  execServe="${execServe} -api-token ${MICROMDM_API_KEY}"
 else
-  SERVER_URL="http://localhost:8080"
+  execServe="${execServe} -api-token ${API_KEY}"
 fi
 
+if [[ ${MICROMDM_NAME} =]]; then
+  execServe="${execServe} -name ${MICROMDM_NAME}"
+else
+  execServe="${execServe} -name production"
+fi
 
-execMdmctl="/usr/local/bin/mdmctl config set -api-token ${API_KEY} -name production -server-url ${SERVER_URL}"
+if [[ ${TLS} = true ]]; then
+  execServe="${execServe} -server-url https://localhost:443"
+else
+  execServe="${execServe} -server-url http://localhost:8080"
+fi
 
 echo "Configuring mdmctl using: $execMdmctl"
 
