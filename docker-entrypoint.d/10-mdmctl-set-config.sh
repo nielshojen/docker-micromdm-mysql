3fef0085e0986e3d40209d6540f628ec100f32c8
Niels Højen <niels@hojen.net>
2023-03-01 08:16:21 +0100

    fix var name

--- docker-entrypoint.d/10-mdmctl-set-config.sh
+++ docker-entrypoint.d/10-mdmctl-set-config.sh
@@ -5,21 +5,21 @@ sleep 5
 execMdmctl="/usr/local/bin/mdmctl config set"
 
 if [[ ${MICROMDM_API_KEY} ]]; then
-  execServe="${execServe} -api-token ${MICROMDM_API_KEY}"
+  execMdmctl="${execServe} -api-token ${MICROMDM_API_KEY}"
 else
-  execServe="${execServe} -api-token ${API_KEY}"
+  execMdmctl="${execServe} -api-token ${API_KEY}"
 fi
 
 if [[ ${MICROMDM_NAME} ]]; then
-  execServe="${execServe} -name ${MICROMDM_NAME}"
+  execMdmctl="${execServe} -name ${MICROMDM_NAME}"
 else
-  execServe="${execServe} -name production"
+  execMdmctl="${execServe} -name production"
 fi
 
 if [[ ${MICROMDM_TLS} = true ]]; then
-  execServe="${execServe} -server-url https://localhost:443"
+  execMdmctl="${execServe} -server-url https://localhost:443"
 else
-  execServe="${execServe} -server-url http://localhost:8080"
+  execMdmctl="${execServe} -server-url http://localhost:8080"
 fi
 
 echo "Configuring mdmctl using: $execMdmctl"
--- docker-entrypoint.d/20-mdmctl-switch-config.sh
+++ docker-entrypoint.d/20-mdmctl-switch-config.sh
@@ -5,9 +5,9 @@ sleep 5
 execMdmctl="/usr/local/bin/mdmctl config switch"
 
 if [[ ${MICROMDM_NAME} ]]; then
-  execServe="${execServe} -name ${MICROMDM_NAME}"
+  execMdmctl="${execServe} -name ${MICROMDM_NAME}"
 else
-  execServe="${execServe} -name production"
+  execMdmctl="${execServe} -name production"
 fi
 
 echo "Switching config using: $execMdmctl"
