64046dd004f68b017dfe76d012ace46966de663c
Niels Højen <niels@hojen.net>
2023-03-01 08:41:04 +0100

    doh

--- docker-entrypoint.d/10-mdmctl-set-config.sh
+++ docker-entrypoint.d/10-mdmctl-set-config.sh
@@ -5,21 +5,21 @@ sleep 5
 execMdmctl="/usr/local/bin/mdmctl config set"
 
 if [[ ${MICROMDM_API_KEY} ]]; then
-  execMdmctl="${execServe} -api-token ${MICROMDM_API_KEY}"
+  execMdmctl="${execMdmctl} -api-token ${MICROMDM_API_KEY}"
 else
-  execMdmctl="${execServe} -api-token ${API_KEY}"
+  execMdmctl="${execMdmctl} -api-token ${API_KEY}"
 fi
 
 if [[ ${MICROMDM_NAME} ]]; then
-  execMdmctl="${execServe} -name ${MICROMDM_NAME}"
+  execMdmctl="${execMdmctl} -name ${MICROMDM_NAME}"
 else
-  execMdmctl="${execServe} -name production"
+  execMdmctl="${execMdmctl} -name production"
 fi
 
 if [[ ${MICROMDM_TLS} = true ]]; then
-  execMdmctl="${execServe} -server-url https://localhost:443"
+  execMdmctl="${execMdmctl} -server-url https://localhost:443"
 else
-  execMdmctl="${execServe} -server-url http://localhost:8080"
+  execMdmctl="${execMdmctl} -server-url http://localhost:8080"
 fi
 
 echo "Configuring mdmctl using: $execMdmctl"
